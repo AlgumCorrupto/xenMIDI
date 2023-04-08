b113f9be70e827c7063744e53d36910304547c8b
AlgumCorrupto <pauloarturvillaca@gmail.com>
2023-04-08 18:53:50 -0300

    home-menu-prototype

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SequencerRootComponent } from './components/sequencer-root/sequencer-root.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavComponent,
+    FooterComponent,
+    SequencerRootComponent,
   ],
   imports: [
     BrowserModule,
