1c442d192c2e3da837da23c2ee9479fb9baf5793
AlgumCorrupto <pauloarturvillaca@gmail.com>
2023-04-11 00:17:02 -0300

    pt2

--- src/graphics/tilemap/tilemap.service.ts
+++ src/graphics/tilemap/tilemap.service.ts
@@ -19,22 +19,48 @@ interface iTileRect {
 })
 export class tilemapService{
     selectedEDO: iNoteGrid = { xDeltaTime: 128, yNotePitch: 128,idTemperament: 12 };
-    tileRect: iTileRect = { xWidth: 32, yHeight: 16 };
+    tileRect: iTileRect = { xWidth: 24, yHeight: 16 };
     public graphics = new PIXI.Graphics();
 
+    //function that draws the grid on the screen (used when initialized)
     public drawGrid(graphics: PIXI.Graphics, app: PIXI.Application)
     {
         const xMax = this.selectedEDO.xDeltaTime * this.tileRect.xWidth;
         const yMax = this.selectedEDO.yNotePitch * this.tileRect.yHeight;
-        graphics.lineStyle(4, 0xFF0000);
+        graphics.lineStyle(1, 0xafb2b6);
 
         for (let x: number = 0; x < xMax; x+= this.tileRect.xWidth)
             for(let y: number = 0; y < yMax; y+= this.tileRect.yHeight) {
                 graphics.drawRect( x, y, this.tileRect.xWidth, this.tileRect.yHeight);
                 app.stage.addChild(graphics);
             }
-            
     }
 
+    //function that draws highlight when you hover your mouse on a cell
+    public drawHighlight(graphics: PIXI.Graphics, app: PIXI.Application, e: MouseEvent) {
+        const xMax = this.selectedEDO.xDeltaTime * this.tileRect.xWidth;
+        const yMax = this.selectedEDO.yNotePitch * this.tileRect.yHeight;
+        graphics.lineStyle(1, 0xd7891c);
+
+        //get mouse coordinates
+        app.stage.interactive = true
+        let mouseX: number = e.clientX;
+        let mouseY: number = e.clientX;
+
+        let gridX: number = this.selectedEDO.xDeltaTime;
+        let gridY: number = this.selectedEDO.yNotePitch;
+        /*find active cell
+        SelCell = [MouseX / xDeltaTime, MouseY / yNotePitch]
+        */
+       
+        selectedCell: [] = [mouseX / gridX, mouseY / gridY];
+        
+        //find offset into cell (later)
+
+
+        //draw the highlight
+        graphics.drawRect(gridX, gridY, this.tileRect.xWidth, this.tileRect.yHeight);
+        app.stage.addChild(graphics);
+    }
 
 }
